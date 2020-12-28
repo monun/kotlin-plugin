38540330a7a48a9f0ca046fdf6d4553aca7002e1
Noonmaru <noonmaru10@gmail.com>
2020-12-29 07:52:31 +0900

    JDK 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
     // kotlinx library
