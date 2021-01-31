7e5e8331117ee28de3880b41a31dd1834d0c7c5a
Monun <monun1010@gmail.com>
2021-01-31 21:07:32 +0900

    Fix

--- src/main/kotlin/com/github/monun/kotlin/KotlinPlugin.kt
+++ src/main/kotlin/com/github/monun/kotlin/KotlinPlugin.kt
@@ -1,13 +1,8 @@
 package com.github.monun.kotlin
 
-import com.github.noonmaru.kommand.kommand
-import com.github.monun.kotlin.util.GitHubSupport
-import com.github.monun.kotlin.util.UpToDateException
+import com.github.monun.kommand.kommand
 import com.github.monun.kotlin.util.updateFromGitHubMagically
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.bukkit.plugin.java.JavaPlugin
-import java.io.File
 
 /**
  * @author Monun
