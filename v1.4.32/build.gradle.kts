3d5a6c74a604bbeb80c520893f3039dc32fa3f89
monun <monun1010@gmail.com>
2021-04-23 15:30:33 +0900

    :arrow_up: Exposed 0.30.2

--- v1.4.32/build.gradle.kts
+++ v1.4.32/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlinx("kotlinx-serialization-json", "1.1.0"))
     implementation(kotlinx("kotlinx-coroutines-core", "1.4.3"))
-    val exposedVersion = "0.29.1"
+    val exposedVersion = "0.30.2"
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
